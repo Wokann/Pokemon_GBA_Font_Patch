67b2c124e35ce3169bb3feb55faae435a8c7fd0a
GitHub Wokann <Kenny_chen1998@outlook.com>
2023-10-09 21:55:41 +0800

    小更新

--- pokeRS/graphic/graphic.s
+++ pokeRS/graphic/graphic.s
@@ -1,8 +1,9 @@
 .org PokeRSFontChsAddress
 .incbin "./graphic/font/PokeRSFontChs(0x200000).bin"
+;.incbin "./graphic/font/PokeRSFontChs(0x200000)_unshadow.bin"
 
 .org PokeRSFontChsSymAddress
-.incbin "./graphic/font/PokeRSFontChsSym(EA39C0-EA3BFF).bin"
+.incbin "./graphic/font/PokeRSFontChsSym(0x240).bin"
 
 .org gPSSMenuHeader_Tilemap
 .incbin "./graphic/pokemon_storage/header.bin.lz"
--- pokeRS/strings/testText.s
+++ pokeRS/strings/testText.s
@@ -1,8 +1,8 @@
-.org BattleText_SafariBalls
-.strn "{HIGHLIGHT}{RED}狩猎球       $"
 ;狩猎球后必须至少7个空格，不然显示出问题，但6个空格时已达原长度上限。
 ;{HIGHLIGHT}{RED}不可丢，会显示问题。
 ;故延后后一个文本的指针，以增加1个空格空间。
+.org BattleText_SafariBalls
+.strn "{HIGHLIGHT}{RED}狩猎球       $"
 
 .org BattleText_SafariBallsLeft + 1
 .strn "{HIGHLIGHT}{RED}剩余:$"
@@ -10,9 +10,9 @@
 .org gMainMenuString_NewGame
 .strn "新游戏$"
 
+;盒子壁纸，可用于测试Getstringwidth函数是否正常
 .org PCText_PokeCenter 
 .strn "宝可梦中心$" 
-;盒子壁纸，可用于测试Getstringwidth函数是否正常
 
 
 

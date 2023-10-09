0b96d6b9f682be6875935c75738317dbc0ffbe1c
GitHub Wokann <Kenny_chen1998@outlook.com>
2023-10-09 22:32:35 +0800

    大小字库拆分

--- pokeRS/graphic/graphic.s
+++ pokeRS/graphic/graphic.s
@@ -1,6 +1,10 @@
-.org PokeRSFontChsAddress
-.incbin "./graphic/font/PokeRSFontChs(0x200000).bin"
-;.incbin "./graphic/font/PokeRSFontChs(0x200000)_unshadow.bin"
+.org PokeRSFontChsNormal
+.incbin "./graphic/font/PokeRSFontChsNormal(0xE0000).bin"
+;.incbin "./graphic/font/PokeRSFontChsNormal_unshadow(0xE0000).bin"
+
+.org PokeRSFontChsSmall
+.incbin "./graphic/font/PokeRSFontChsSmall(0xE0000).bin"
+;.incbin "./graphic/font/PokeRSFontChsSmall_unshadow(0xE0000).bin"
 
 .org PokeRSFontChsSymAddress
 .incbin "./graphic/font/PokeRSFontChsSym(0x240).bin"
--- pokeRS/include/hack_R.s
+++ pokeRS/include/hack_R.s
@@ -1,7 +1,6 @@
 ;HackFunctionAddress
 HackStart                       equ 0x082089dC
 PokeRSFontChsSymAddress         equ 0x08EA39C0
-PokeRSFontChsAddress            equ 0x09000000
 PokeRSFontChsNormal             equ 0x09000000
 PokeRSFontChsSmall              equ 0x09100000
 

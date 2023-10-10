edd39aed497633d3dfa29c58f7cc0fd93590a867
GitHub Wokann <Kenny_chen1998@outlook.com>
2023-10-10 19:18:11 +0800

    火叶增益版-原始版

---
+++ pokeFRLG/include/hack_FR.s
@@ -0,0 +1,6 @@
+;HackFunctionAddress
+HackStart                       equ 0x0820f658
+;PokeFRLGEFontChsSymAddress         equ 0x08EA39C0
+PokeFRLGEFontChsNormal             equ 0x09000000
+PokeFRLGEFontChsSmall              equ 0x09080000
+
---
+++ pokeFRLG/main_FR.asm
@@ -0,0 +1,27 @@
+.gba
+.thumb
+.loadtable "./charmap.txt"
+.create "./chsfontrom_FR.gba",0x08000000
+.close
+.open "./baserom_FR.gba","./chsfontrom_FR.gba",0x08000000
+
+;定义函数地址
+.include "./include/OriginSymbols_FR.s"
+.include "./include/hack_FR.s"
+
+;原址修改的部分
+.include "./src/HackInOrigin.s"
+
+;中文相关函数
+.org HackStart  ;sFontMaleLatinGlyphs + 0x40
+;.include "./src/GetGlyphWidthChinese.s"
+;.include "./src/GetStringWidthChinese.s"
+.include "./src/RenderTextChinese.s"
+
+;图片及中文字模
+;.include "./graphic/graphic.s"
+
+;测试文本
+;.include "./strings/testText.s"
+
+.close

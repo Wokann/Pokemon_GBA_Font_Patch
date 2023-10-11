ad131dfc5278ec4712edcae70a89d231d6cbe63d
GitHub Wokann <Kenny_chen1998@outlook.com>
2023-10-11 16:18:59 +0800

    火叶增益优化版同步

--- pokeFRLG/graphic/graphic.s
+++ pokeFRLG/graphic/graphic.s
@@ -1,18 +1,18 @@
-.org PokeFRLGEFontChsNormal
-.incbin "./graphic/fonts/PokeFRLGEFontChsNormal(0x70000).bin"
+.org PokeFRLGFontChsNormal
+.incbin "./graphic/fonts/PokeFRLGFontChsNormal(0x70000).bin"
 
-.org PokeFRLGEFontChsSmall
-.incbin "./graphic/fonts/PokeFRLGEFontChsSmall(0x70000).bin"
+.org PokeFRLGFontChsSmall
+.incbin "./graphic/fonts/PokeFRLGFontChsSmall(0x70000).bin"
 
-.org PokeFRLGEFontChsSymAddress1
-.incbin "./graphic/fonts/PokeFRLGEFontChsSym(0x240).bin"
+.org PokeFRLGFontChsSymAddress1
+.incbin "./graphic/fonts/PokeFRLGFontChsSym(0x240).bin"
 
-.org PokeFRLGEFontChsSymAddress2
-.incbin "./graphic/fonts/PokeFRLGEFontChsSym(0x240).bin"
+.org PokeFRLGFontChsSymAddress2
+.incbin "./graphic/fonts/PokeFRLGFontChsSym(0x240).bin"
 
-.org PokeFRLGEFontChsSymAddress3
-.incbin "./graphic/fonts/PokeFRLGEFontChsSym(0x240).bin"
+.org PokeFRLGFontChsSymAddress3
+.incbin "./graphic/fonts/PokeFRLGFontChsSym(0x240).bin"
 
-.org PokeFRLGEFontChsSymAddress4
-.incbin "./graphic/fonts/PokeFRLGEFontChsSym(0x240).bin"
+.org PokeFRLGFontChsSymAddress4
+.incbin "./graphic/fonts/PokeFRLGFontChsSym(0x240).bin"
 

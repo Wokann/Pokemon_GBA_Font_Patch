a57e5cb1b8760051df9c6e573c320b6e6b70157b
GitHub Wokann <Kenny_chen1998@outlook.com>
2023-10-11 18:07:43 +0800

    清理代码

--- old_(PMxxUS_CHPLUS_RELEASE)/PMFRUS_CHPLUS_RELEASE3/graphic/graphic.s
+++ old_(PMxxUS_CHPLUS_RELEASE)/PMFRUS_CHPLUS_RELEASE3/graphic/graphic.s
@@ -3,17 +3,3 @@
 
 .org PokeFRLGFontChsSmall
 .incbin "./graphic/fonts/PokeFRLGFontChsSmall(0x70000).bin"
-/*
-.org PokeFRLGFontChsSymAddress1
-.incbin "./graphic/fonts/PokeFRLGFontChsSym(0x240).bin"
-
-.org PokeFRLGFontChsSymAddress2
-.incbin "./graphic/fonts/PokeFRLGFontChsSym(0x240).bin"
-
-.org PokeFRLGFontChsSymAddress3
-.incbin "./graphic/fonts/PokeFRLGFontChsSym(0x240).bin"
-
-.org PokeFRLGFontChsSymAddress4
-.incbin "./graphic/fonts/PokeFRLGFontChsSym(0x240).bin"
-
-*/

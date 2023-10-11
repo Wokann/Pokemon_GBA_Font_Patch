b4be1f27e57a54b782a49312258f42b6c1dbee72
GitHub Wokann <Kenny_chen1998@outlook.com>
2023-10-11 10:28:14 +0800

    迁移armips位置

--- old_(PMxxUS_CHPLUS_RELEASE)/PMFRUS_CHPLUS_RELEASE3/build.bat
+++ old_(PMxxUS_CHPLUS_RELEASE)/PMFRUS_CHPLUS_RELEASE3/build.bat
@@ -1,3 +1,3 @@
-..\..\tools\armips\armips.exe main_PMFRUS_CHPLUS_RELEASE3.asm
+.\tools\armips\armips.exe main_PMFRUS_CHPLUS_RELEASE3.asm
 @echo off
 ping 127.0.0.1 -n 3 >nul
--- pokeRS/build.bat
+++ pokeRS/build.bat
@@ -1,4 +1,4 @@
-..\tools\armips\armips.exe main_R.asm
-..\tools\armips\armips.exe main_S.asm
+.\tools\armips\armips.exe main_R.asm
+.\tools\armips\armips.exe main_S.asm
 @echo off
 ping 127.0.0.1 -n 3 >nul
